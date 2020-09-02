6dccdf247d932f7e882646aacfce88ce25d70c27
Olga <olga4444444@mail.ru>
2020-09-02 16:56:07 +0500

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -16,19 +22,31 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("./build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("./build"))
+    .pipe(sync.stream());
+}
+exports.html = html;
+
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: './build'
     },
     cors: true,
     notify: false,
@@ -47,5 +65,82 @@ const watcher = () => {
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  styles, html, server, watcher
 );
+
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg, png, svg}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.mozjpeg({proggressive: true}),
+        imagemin.svgo()
+    ]))
+}
+
+exports.images = images;
+
+// Convert to WebP
+
+const createWebP = () => {
+    return gulp.src("source/img/**/*.{jpg, png}")
+     .pipe(webp({quality: 90}))
+     .pipe(gulp.dest("source/img"))
+}
+
+exports.createWebP = createWebP;
+
+
+// Sprite
+
+const sprite = () => {
+   return gulp.src("source/img/**/icon-*.svg")
+      .pipe(svgstore())
+      .pipe(rename("sprite.svg"))
+      .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+      base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+// del
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
+
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  styles,
+  images,
+  sprite,
+  html
+);
+
+exports.build = build;
+
+exports.start = gulp.series(build, server, watcher);
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -6,15 +6,15 @@ navMain.classList.remove('main-nav-nojs');
 
 navToggle.addEventListener('click', function() {
 
- if (navMain.classList.contains('main-nav-closed')) {
-   navMain.classList.remove('main-nav-closed');
-   navMain.classList.add('main-nav-opened');
-   pageHeader.style.backgroundColor = '#283645';
- }
-   else {
-     navMain.classList.add('main-nav-closed');
-     navMain.classList.remove('main-nav-opened');
-     pageHeader.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-   }
- }
+  if (navMain.classList.contains('main-nav-closed')) {
+    navMain.classList.remove('main-nav-closed');
+    navMain.classList.add('main-nav-opened');
+    pageHeader.style.backgroundColor = '#283645';
+  }
+    else {
+      navMain.classList.add('main-nav-closed');
+      navMain.classList.remove('main-nav-opened');
+      pageHeader.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    }
+  }
 );
